4b027b83b6ead85a50333221b2ae2f3fbbfd4899
Abdulhaseeb101 <hello.abdulhaseeb@gmail.com>
2023-03-23 10:19:28 +0530

    change admin to tabular

--- cookie_proj_ui/lib/ConsentPage.dart
+++ cookie_proj_ui/lib/ConsentPage.dart
@@ -1,5 +1,6 @@
 import 'package:cookie_proj_ui/ConsentInfoPage.dart';
 import 'package:cookie_proj_ui/ConsentRepository.dart';
+import 'package:cookie_proj_ui/models/Consent.dart';
 import 'package:flutter/material.dart';
 
 class ConsentPage extends StatefulWidget {
@@ -21,42 +22,88 @@ class _ConsentPageState extends State<ConsentPage> {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: Column(
-        children: [
-          FutureBuilder(
-            future: ConsentRepository().getAllConsents(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return Text(snapshot.error.toString());
-                }
-                return ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: const Icon(Icons.perm_identity),
-                      title: Text(snapshot.data![index].id),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ConsentInfoPage(
-                                    consentId: snapshot.data![index].id,
-                                  )),
-                        ).then((value) => setState(() {}));
-                      },
-                    );
-                  },
-                );
-              } else {
-                return const CircularProgressIndicator();
+      body: Center(
+        child: FutureBuilder(
+          future: ConsentRepository().getAllConsents(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
               }
-            },
-          ),
-        ],
+              // return ListView.builder(
+              //   shrinkWrap: true,
+              //   scrollDirection: Axis.vertical,
+              //   itemCount: snapshot.data!.length,
+              //   itemBuilder: (context, index) {
+              //     return ListTile(
+              //       leading: const Icon(Icons.perm_identity),
+              //       title: Text(snapshot.data![index].id),
+              //       onTap: () {
+              //         Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //               builder: (context) => ConsentInfoPage(
+              //                     consentId: snapshot.data![index].id,
+              //                   )),
+              //         ).then((value) => setState(() {}));
+              //       },
+              //     );
+              //   },
+              // );
+              return CustomDataTable(allConsents: snapshot.data!);
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
       ),
     );
+  }
+}
+
+class CustomDataTable extends StatefulWidget {
+  final List<Consent> allConsents;
+  const CustomDataTable({
+    super.key,
+    required this.allConsents,
+  });
+
+  @override
+  State<CustomDataTable> createState() => _CustomDataTableState();
+}
+
+class _CustomDataTableState extends State<CustomDataTable> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        children: [
+          DataTable(
+            columns: const [
+              DataColumn(label: Text("Consent Id")),
+              DataColumn(label: Text("Timestamp")),
+              DataColumn(label: Text("Ip Address")),
+            ],
+            rows: widget.allConsents
+                .map((con) => DataRow(
+                      cells: [
+                        DataCell(Text(con.id),
+                            onTap: () => {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => ConsentInfoPage(
+                                              consentId: con.id,
+                                            )),
+                                  ).then((value) => setState(() {}))
+                                }),
+                        DataCell(Text(con.timestamp)),
+                        DataCell(Text(con.ipaddr)),
+                      ],
+                    ))
+                .toList(),
+          ),
+        ]);
   }
 }
