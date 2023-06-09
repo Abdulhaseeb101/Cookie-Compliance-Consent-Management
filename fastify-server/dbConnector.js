17ce5e864d5ddf0215be274b36e2b3d1ab016dff
Abdulhaseeb101 <33460217+Abdulhaseeb101@users.noreply.github.com>
2023-06-09 12:04:45 +0530

    Update dbConnector.js

--- fastify-server/dbConnector.js
+++ fastify-server/dbConnector.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function dbConnector(fastify, options) {
   // Connection URL
   const mongoDBURL =
-    "mongodb+srv://strangecat:jA7OwMJhALQhp8H6@cookieconsentdb.dhesh1t.mongodb.net/?retryWrites=true&w=majority";
+    "ADD MongoDb URL HERE";
   mongoose.connect(mongoDBURL);
 }
 
